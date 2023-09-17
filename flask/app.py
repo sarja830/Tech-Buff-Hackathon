9669c05274a5b3641199a3345f641352444db47a
Ankit Shaw <25771394+ankitshaw@users.noreply.github.com>
2023-09-17 22:32:03 +0000

    up

--- flask/app.py
+++ flask/app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 # json="Give an answer in json format. The json string should have a key Actionable_Steps with value as json object list. Each object in the list is one step and include the step number, category, tasks, team positions , resources."
 # prefix= "Act like a venture coach and mentor and give me actionable steps with points to build my start from scratch which is "
 # suffix= " Give an answer in json format and also give me Resources in the Niagara Falls and Buffalo Area. I also need different positions I would need in my team for each step."
-prefix = "Act like a venture coach and give me 8 actionable steps in json format which includes the step number, category, multiple elaborated tasks, team positions, multiple resources that can help at each step like organizations, funding, incubation centers to help me in each step located around Niagara Falls, Buffalo (provide website links if possible) for my startup. Start up idea is: "
+prefix = "Act like a venture coach and give me 8 actionable steps in json format which includes the step number, category, multiple elaborated tasks, team positions, multiple resources that can help at each step like organizations, to help me in each step located around Niagara Falls, Buffalo (provide website links if possible) for my startup. Start up idea is: "
 
 plan_json = None
 
@@ -39,12 +39,12 @@ def getbplan():
 
 
  
-# main driver function
-if __name__ == '__main__':
+# # main driver function
+# if __name__ == '__main__':
  
-    # run() method of Flask class runs the application
-    # on the local development server.
-    app.run()
+#     # run() method of Flask class runs the application
+#     # on the local development server.
+#     app.run()
 
 
 # prompt="By 2026, more than 217 million people in the U.S. will use online food delivery services. Tap into that market and start a meal-prep service to make people’s lives easier and cater to specialized diets (keto, vegan, Whole30)."
@@ -58,13 +58,13 @@ if __name__ == '__main__':
 
 # prompt = "Implement blockchain technology to enhance transparency and traceability in supply chains. Your startup can create a platform that helps companies track the movement of goods from manufacturers to consumers, ensuring authenticity and reducing the risk of counterfeit products. Blockchain can also streamline transactions and reduce fraud. "
 # prefix_final = "Act like a venture coach and give me 8 actionable steps in json format which includes the step number, category, multiple elaborated tasks, team positions, resources like organizations, funds to help me in each step located around Niagara Falls, Buffalo (provide website links if possible) for my startup. Start up idea is to "
-# prompt = "By 2026, more than 217 million people in the U.S. will use online food delivery services. Tap into that market and start a meal-prep service to make people’s lives easier and cater to specialized diets (keto, vegan, Whole30)."
+prompt = "By 2026, more than 217 million people in the U.S. will use online food delivery services. Tap into that market and start a meal-prep service to make people’s lives easier and cater to specialized diets (keto, vegan, Whole30)."
 
 
-# response = openai.ChatCompletion.create(
-#         model="gpt-3.5-turbo-16k",
-#         messages=[
-#             {"role": "user", "content": prefix+prompt}
-#         ]
-#     )
-# print(response['choices'][0]['message']['content'])
+response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo-16k",
+        messages=[
+            {"role": "user", "content": prefix+prompt}
+        ]
+    )
+print(response['choices'][0]['message']['content'])
